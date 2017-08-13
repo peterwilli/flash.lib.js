562e73fae155c06e84cf95988944610cd7079c9e
Peter <peter@codebuffet.co>
2017-08-13 12:49:12 +0200

    started on generate state

--- src/main.js
+++ src/main.js
@@ -1,3 +1,29 @@
+import { seedGen } from "./utils"
+
 export class Flash {
-  
+  state = {}
+
+  initialize(maxAmountOfTransactions, depositAmount, settlementAddress, securityLevel) {
+    this.state = generateState(depositAmount, settlementAddress, securityLevel)
+  }
+
+  join() {
+
+  }
+
+  static generateState(depositAmount, settlementAddress, securityLevel) {
+    var myseed = seedGen(81)
+    return {
+      mySeed,
+      publicLedger: {
+        remainderAddress: iota.multisig.getDigest(myseed, 0, securityLevel)
+        depositAmount: depositAmount
+        addressIndex: 1,
+        stakes: [],
+        balances: [],
+        settlementAddresses: [settlementAddress],
+        securityLevels: [securityLevel],
+      }
+    }
+  }
 }
---
+++ src/utils.js
@@ -0,0 +1,16 @@
+export const seedGen = length => {
+  var charset = "ABCDEFGHIJKLMNOPQRSTUVWXYZ9";
+  var i;
+  var result = "";
+  if (window.crypto && window.crypto.getRandomValues) {
+    var values = new Uint32Array(length);
+    window.crypto.getRandomValues(values);
+    for (i = 0; i < length; i++) {
+      result += charset[values[i] % charset.length];
+    }
+    return result;
+  } else
+    throw new Error(
+      "Your browser is too old to generate secure random seeds."
+    );
+};
